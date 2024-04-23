7b51fef2d72f33746263e30e77a430f885144ff5
Daniel Rojo <danielrojo@gmail.com>
2024-04-23 12:59:20 +0200

    heros with description

--- src/app/components/heroes/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes/heroes.component.ts
@@ -1,25 +1,29 @@
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Hero } from '../../../models/hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor, JsonPipe, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
 
-  heroes = [ 'Windstorm', 'Bombasto', 'Magneta', 'Tornado' ]
+  heroes: Hero[] = [ new Hero('Superman', 'man of steel'), new Hero('Batman', 'dark knight') ]
 
-  heroName = 'Batman'
+  heroName = ''
+  heroDescription = ''
 
   constructor() { }
 
   addHero() {
     if (this.heroName) {
-      this.heroes.push(this.heroName)
+      this.heroes.push(new Hero(this.heroName, this.heroDescription))
       this.heroName = ''
+      this.heroDescription = ''
     }
   }
 }
---
+++ src/app/models/hero.ts
@@ -0,0 +1,11 @@
+export class Hero {
+    constructor(private _name: string, private _description = '') {
+    }
+    get name() {
+        return this._name;
+    }
+
+    get description() {
+        return this._description;
+    }
+}
